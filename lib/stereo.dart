b459e47e33a68af32dd4f88f27ebb8b64522826d
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-08-29 22:21:51 +0200

    Renamed AudioPlayer class to Stereo (#6)

--- lib/stereo.dart
+++ lib/stereo.dart
@@ -5,30 +5,37 @@ import 'package:flutter/services.dart';
 
 /// Represents an audio player.
 class Stereo {
-  static const MethodChannel _channel =
-  const MethodChannel('com.mcs.plugins/stereo');
+  static Stereo _instance = new Stereo._internal();
 
-  Stereo._internal() {
-    Stereo._channel.setMethodCallHandler(Stereo._handleMethodCall);
-  }
+  static const MethodChannel _channel =
+      const MethodChannel('com.mcs.plugins/stereo');
 
   static VoidCallback togglePlayPauseCallback;
 
+  factory Stereo() {
+    return _instance;
+  }
+
+  Stereo._internal() {
+    _channel.setMethodCallHandler(_handleMethodCall);
+  }
+
   /// Always returns `0`.
-  static Future<int> loadItemWithURL(String url) =>
-      _channel.invokeMethod('app.loadItemWithURL', url);
+  Future<int> loadItemWithURL(String url) {
+    return _channel.invokeMethod('app.loadItemWithURL', url);
+  }
 
   /// Returns information about the song the user picked.
-  static Future<String> showMediaPicker() {
+  Future<String> showMediaPicker() {
     return _channel.invokeMethod('app.showMediaPicker');
   }
 
   /// Returns `true` if the player resumed playing, `false` otherwise.
-  static Future<bool> togglePlaying() {
+  Future<bool> togglePlaying() {
     return _channel.invokeMethod('app.togglePlaying');
   }
 
-  static Future _handleMethodCall(MethodCall call) async {
+  Future _handleMethodCall(MethodCall call) async {
     switch (call.method) {
       case 'event.togglePlayPause':
         togglePlayPauseCallback();
